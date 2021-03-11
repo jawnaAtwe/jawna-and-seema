7e426a8579d9cdec2204a9dc99bd9ca7b0ded36d
SEMO <SEMO@192.168.1.57>
2021-03-11 22:49:05 +0200

    Add Final

--- src/test/java/bookproject/CheckAdd.java
+++ src/test/java/bookproject/CheckAdd.java
@@ -3,6 +3,8 @@ package bookproject;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JOptionPane;
 
@@ -13,33 +15,29 @@ import io.cucumber.java.en.When;
 public class CheckAdd {
 	
 	public Add Book1;
-	ArrayList<Add>BookList= new ArrayList<Add>();
 	private Fruit fc1;
-	public boolean b=false;
-	public static String name="seema",pass="123s"; // Enter UserName & Password
+	public boolean b=false, b2=false;
 
 	public CheckAdd(Fruit fc1) {
 		this.fc1 = fc1;
 	}
-	
-	@Given("the admin must have logged in")
-	public void the_admin_must_have_logged_in() {
-		
-		b=fc1.Check(name,pass);
-		
+
+
+	@Given("the admin must have loged in")
+	public void the_admin_must_have_loged_in() {
+		b=fc1.Checkpass();
 	}
 
+	
 	@When("the admin fill ISBN {string} , title {string} , author {string} and signature {string}")
 	public void the_admin_fill_isbn_title_author_and_signature
 	( String ISBN, String Title, String Author, String Signature) {
 		
-		if((ISBN.equalsIgnoreCase("")&&Title.equalsIgnoreCase("")&&Author.equalsIgnoreCase("")
-				&&Signature.equalsIgnoreCase(""))==false) {
-
-		Book1 =new Add(ISBN,Title,Author,Signature);
-		}
-		
-		else JOptionPane.showMessageDialog(null, "There is text not fill!");
+	 if((ISBN.equalsIgnoreCase("")&&Title.equalsIgnoreCase("")&&Author.equalsIgnoreCase("")&&Signature.equalsIgnoreCase(""))==false) 
+	 {
+		 Book1 =new Add(ISBN,Title,Author,Signature); }
+	
+	 else JOptionPane.showMessageDialog(null, "There is text not fill!");
 		
 	}
 	
@@ -53,34 +51,29 @@ public class CheckAdd {
 	@Then("the book added to the library")
 	public void the_book_added_to_the_library() {
 		
-		BookList.add(Book1);
+		fc1.addbook(Book1);
 		
-		for (int p=0;p<BookList.size();p++) {     
-		  System.out.println(BookList.get(p).getISBN()+" \t"+BookList.get(p).getTitle()+"\t \t"
-			+BookList.get(p).getAuthor()+"\t  \t"+BookList.get(p).getSignature());
-		}
 	}
 
 	@Then("Show a message The book was added successfully")
 	public void show_a_message_the_book_was_added_successfully() {
-			
-		assertTrue(b); 
+		
+		b2 = fc1.booleanadd();
+		assertTrue(b&&b2); 
 		JOptionPane.showMessageDialog(null, "The book was added successfully *_^ ");
 	
 	  
 	}
 	
-	@Given("the admin did not logged in")
-	public void the_admin_did_not_logged_in() {
-		
-		b=fc1.Check(name,pass);
-		
+	@Given("the admin did not loged in")
+	public void the_admin_did_not_loged_in() {
+		b=fc1.Checkpass();
 	}
 	
 	@Then("the book will not add to the library")
 	public void the_book_will_not_add_to_the_library() {
 		
-		assertTrue(!b);
+		assertTrue(b==false);
 	   
 	}
 
