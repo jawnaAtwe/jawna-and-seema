eb06d143503df53a02ad987d68f60c6d228173ce
SEMO <SEMO@192.168.1.57>
2021-03-11 22:45:58 +0200

    LogIn Final

--- src/main/java/bookproject/Fruit.java
+++ src/main/java/bookproject/Fruit.java
@@ -2,40 +2,61 @@ package bookproject;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Fruit {
 	
-	public static String nameT="seema",passT="123s"; // UserName & password 
+	public static ArrayList<Add> bookList= new ArrayList<Add>();
+	int A=0;
+	public static String nameT="seema",passT="123s",n="",p=""; // UserName & password 
+	public boolean b=false,c=false,ch=false;
 	
-	public boolean b;
-
 	public static void main(String[] args) {
 		
 	}
 
-
-	public void Load(String name, String pass) {
-		nameT=name;
-		passT=pass;	
-	}
-
-	public boolean Checkpass() {
-		
-		b=(nameT.equalsIgnoreCase("")==false)&&(passT.equalsIgnoreCase("")==false);
-		
-		return (b);
-	}
-	public void log() {
-		// TODO Auto-generated method stub
-		nameT="";
-		passT="";
-	}
-	
 	public boolean Check(String string, String string2) {
 
 		b=(nameT.equalsIgnoreCase(string))&&(passT.equalsIgnoreCase(string2));
+		
+			if(b) {           //save logIn information
+				n=string;
+			    p=string2;}
+		
 		return (b);
 		
 	}
+	
+	public boolean Checkpass() {
+		c=((n.equalsIgnoreCase(nameT))&&(p.equalsIgnoreCase(passT)));
+		return (c);
+	}
 
+	public void addbook(Add book1) {
+		
+		A=0;
+	for(int i=0;i<bookList.size();i++) {
+	if(bookList.get(i).getISBN().equalsIgnoreCase(book1.getISBN())) {	
+		
+		A=1;
+	}
+		
+	}
+	
+	if(A==0)
+		bookList.add(book1);
+	}
+
+
+	
+	public boolean booleanadd() {
+		if (A==0)
+		return true;
+		else {
+			ch=Checkpass();
+			if(ch)
+			JOptionPane.showMessageDialog(null, "This book is already added!");
+			return false;	
+	}}
 
 }
---
+++ uses_case/1login.feature
@@ -0,0 +1,19 @@
+Feature: login
+Background:
+When admain fill "seema" and "123s"
+
+Scenario: Admin can log in with valid credentials
+Given admain want to login to the application
+When admain fill strings
+And admain click login button and it is valid
+Then admain should be loged in and open next page
+
+ 
+Scenario: Admin has the wrong password
+Given admain want to login to the application
+When admain fill strings
+And admain click login button and it is invalid
+Then admain should not be loged in 
+And Show a message invalid password
+And clear password
+
