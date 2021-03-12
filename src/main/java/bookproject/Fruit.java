483d5e4988db7a25d2b85a8dd6d237b0a6215582
SEMO <SEMO@192.168.1.57>
2021-03-12 04:36:24 +0200

    LogIn Final#

--- src/main/java/bookproject/Fruit.java
+++ src/main/java/bookproject/Fruit.java
@@ -2,35 +2,37 @@ package bookproject;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Fruit {
 	
-	
-	
-	public static String nameT="",passT="";
-	public boolean b;
-
+	public static ArrayList<Add> bookList= new ArrayList<Add>();
+	int A=0;
+	public static String nameT="seema",passT="123s",n="",p=""; // UserName & password 
+	public boolean b=false,c=false,ch=false;
 	
 	public static void main(String[] args) {
 		
 	}
 
+	public boolean Check(String string, String string2) {
 
-	public void Load(String name, String pass) {
-		nameT=name;
-		passT=pass;	
-	}
-
-	public boolean Checkpass() {
+		b=(nameT.equalsIgnoreCase(string))&&(passT.equalsIgnoreCase(string2));
 		
-		b=(nameT.equalsIgnoreCase("")==false)&&(passT.equalsIgnoreCase("")==false);
+			if(b) {           //save logIn information
+				n=string;
+			    p=string2;}
 		
 		return (b);
+		
 	}
-	public void log() {
-		// TODO Auto-generated method stub
-		nameT="";
-		passT="";
+	
+	public boolean Checkpass() {
+		c=((n.equalsIgnoreCase(nameT))&&(p.equalsIgnoreCase(passT)));
+		return (c);
 	}
 
+	
 
 }
+
--- uses_case/1Login.feature
+++ uses_case/1Login.feature
@@ -1,23 +1,18 @@
 Feature: login
-Scenario Outline: Admin can log in with valid credentials
+Background:
+When admain fill "seema" and "123s"
+
+Scenario: Admin can log in with valid credentials
 Given admain want to login to the application
-When admain fill <username> and <password> 
+When admain fill strings
 And admain click login button and it is valid
 Then admain should be loged in and open next page
+
  
- Examples:
- |username|password|
- |"seema"|"123s"|
- |"jawna"|"123j"|
- 
-Scenario Outline: Admin has the wrong password
-Given admain want to login page to the application
-When admain fill <username> and <password>
+Scenario: Admin has the wrong password
+Given admain want to login to the application
+When admain fill strings
 And admain click login button and it is invalid
 Then admain should not be loged in 
 And Show a message invalid password
 And clear password
-
-Examples:
- |username|password|
- |"seema"|"123s"|
